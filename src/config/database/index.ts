dcbf6d5e6e77a741c581ef2b849aec17800df71e
Obaid <obaid3035@gmail.com>
2022-03-30 15:19:34 +0500

    H

--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -4,11 +4,14 @@ const AppDataSource = new DataSource({
   type: "postgres",
   url: "postgres://pwmhszjhwxpvit:99321ac75479a76af5141b63a5b38b210a8ac3a00b545d77b05578a7f9e69950@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d6ag5tpu52r1gg",
   logging: false,
-  synchronize: true,
+  synchronize: false,
   entities: [`src/entities/**/*{.ts,.js}`],
   migrations: [
     "src/migrations/**/*{.ts,.js}"
   ],
+  ssl: {
+    rejectUnauthorized: true
+  }
 })
 
 export default AppDataSource;
