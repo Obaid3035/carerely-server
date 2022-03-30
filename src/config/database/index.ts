29c62a313c47d5acfb1668567947f03f0147adef
Obaid <obaid3035@gmail.com>
2022-03-30 15:00:23 +0500

    Heroku

--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm"
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  url: "postgres://obaidaqeel:password@localhost:5432/carerely_development",
+  url: "postgres://pwmhszjhwxpvit:99321ac75479a76af5141b63a5b38b210a8ac3a00b545d77b05578a7f9e69950@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d6ag5tpu52r1gg",
   logging: false,
   synchronize: true,
   entities: [`src/entities/**/*{.ts,.js}`],
