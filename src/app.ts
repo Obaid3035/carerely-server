3b434cf54e161f3965fd6142f13b48f83adc086b
Obaid <obaid3035@gmail.com>
2022-06-16 16:19:36 +0500

    Heroku

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,8 @@ class App {
     new EventHandler(
       new Server(server, {
         cors: {
-          origin: "https://careraly-server.herokuapp.com",
+          // origin: "https://careraly-server.herokuapp.com",
+          origin: process.env.CLIENT
         },
         pingTimeout: 20000,
         pingInterval: 25000
@@ -80,28 +81,3 @@ class App {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
